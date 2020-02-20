585f006f571e919ca856a2351c0a67e1ddf75e1e
Michael Nitschinger <michael@nitschinger.at>
2020-02-20 22:56:45 +0100

    Make an actual n1ql query

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use couchbase::{QueryOptions, Cluster};
 use log::debug;
 use std::error::Error;
 use structopt::StructOpt;
 use warp::Filter;
-use couchbase::Cluster;
+use nu_protocol::{Value, UntaggedValue, Signature};
+use nu_errors::ShellError;
+use nu::{CommandRegistry, CommandArgs, OutputStream};
+use std::sync::Arc;
+use futures::executor::{block_on, block_on_stream};
+use futures::stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -11,16 +17,59 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let opt = CliOptions::from_args();
     debug!("Effective {:?}", opt);
 
-    let _cluster = Cluster::connect(opt.connection_string, opt.username, opt.password);
+    let cluster = Arc::new(Cluster::connect(opt.connection_string, opt.username, opt.password));
 
     if opt.ui {
         tokio::task::spawn(async {
-            let hello = warp::path!("hello" / String).map(|name| format!("Couchbase says, {}!", name));
+            let hello =
+                warp::path!("hello" / String).map(|name| format!("Couchbase says, {}!", name));
             warp::serve(hello).run(([127, 0, 0, 1], 1908)).await;
         });
     }
 
-    nu::cli().await
+    let mut syncer = nu::EnvironmentSyncer::new();
+    let mut context = nu::create_default_context(&mut syncer)?;
+    context.add_commands(vec![nu::whole_stream_command(Query::new(cluster.clone()))]);
+
+    nu::cli(Some(syncer), Some(context)).await
+}
+
+struct Query {
+    cluster: Arc<Cluster>,
+}
+
+impl Query {
+    pub fn new(cluster: Arc<Cluster>) -> Self {
+        Self { cluster }
+    }
+}
+
+impl nu::WholeStreamCommand for Query {
+    fn name(&self) -> &str {
+        "query"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build("query")
+    }
+
+    fn usage(&self) -> &str {
+        "Performs a N1QL query"
+    }
+
+    fn run(
+        &self,
+        args: CommandArgs,
+        registry: &CommandRegistry,
+    ) -> Result<OutputStream, ShellError> {
+        let mut result = block_on(self.cluster.query("select 1=1", QueryOptions::default())).unwrap();
+        let stream = result.rows::<serde_json::Value>().map(|v| {
+            // this is just a prototype...
+            let raw = serde_json::to_string(&v.unwrap()).unwrap();
+            raw.into()
+        });
+        Ok(OutputStream::from_input(stream))
+    }
 }
 
 #[derive(Debug, StructOpt)]
@@ -37,16 +86,8 @@ struct CliOptions {
     connection_string: String,
     #[structopt(long = "ui")]
     ui: bool,
-    #[structopt(
-        short = "u",
-        long = "username",
-        default_value = "Administrator"
-    )]
+    #[structopt(short = "u", long = "username", default_value = "Administrator")]
     username: String,
-    #[structopt(
-        short = "p",
-        long = "password",
-        default_value = "password"
-    )]
+    #[structopt(short = "p", long = "password", default_value = "password")]
     password: String,
 }
