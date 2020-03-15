be14d306df9cf2a88f7ca797bb95d9d053bdcfa1
Michael Nitschinger <michael@nitschinger.at>
2020-03-15 08:51:20 +0100

    More fake features

--- src/cli/fake_data/mod.rs
+++ src/cli/fake_data/mod.rs
@@ -1,5 +1,9 @@
 use super::util::convert_json_value_to_nu_value;
 use crate::state::State;
+use fake::faker::internet::raw::*;
+use fake::faker::name::raw::*;
+use fake::locales::*;
+use fake::Fake;
 use futures::executor::block_on;
 use nu_cli::{CommandArgs, CommandRegistry, OutputStream};
 use nu_errors::ShellError;
@@ -26,7 +30,19 @@ impl nu_cli::WholeStreamCommand for FakeData {
     }
 
     fn signature(&self) -> Signature {
-        Signature::build("fake").named("template", SyntaxShape::Path, "path to the template", None)
+        Signature::build("fake")
+            .named("template", SyntaxShape::Path, "path to the template", None)
+            .named(
+                "num-rows",
+                SyntaxShape::Int,
+                "number of rows to generate",
+                None,
+            )
+            .switch(
+                "list-functions",
+                "List all functions currently registered",
+                None,
+            )
     }
 
     fn usage(&self) -> &str {
@@ -49,41 +65,70 @@ async fn run_fake(
 ) -> Result<OutputStream, ShellError> {
     let args = args.evaluate_once(registry)?;
 
-    let path = args.get("template").ok_or_else(|| {
-        ShellError::labeled_error(
-            "No file or directory specified",
-            "for command",
-            Tag::default(),
-        )
-    })?;
-
-    let path = path.as_path().unwrap();
-    let template = fs::read_to_string(path).unwrap();
+    let list_functions = args.get("list-functions").is_some();
 
     let ctx = Context::new();
     let mut tera = Tera::default();
 
+    tera.register_function("uuid", fake_uuid);
     tera.register_function("name", fake_name);
     tera.register_function("firstName", fake_first_name);
     tera.register_function("lastName", fake_last_name);
     tera.register_function("title", fake_title);
     tera.register_function("nameWithTitle", fake_name_with_title);
-    tera.register_function("username", fake_username);
+    tera.register_function("userName", fake_username);
 
     let mut results = Vec::new();
 
-    let generated = tera.render_str(&template, &ctx).unwrap();
-    let content = serde_json::from_str(&generated).unwrap();
-    let content_converted = convert_json_value_to_nu_value(&content, Tag::default());
-    results.push(content_converted);
+    if list_functions {
+        let generated = tera.render_str(LIST_FUNCTIONS, &ctx).unwrap();
+        let content = serde_json::from_str(&generated).unwrap();
+        match content {
+            serde_json::Value::Array(values) => {
+                for value in values {
+                    let content_converted = convert_json_value_to_nu_value(&value, Tag::default());
+                    results.push(content_converted);
+                }
+            }
+            _ => unimplemented!(),
+        }
+    } else {
+        let path = args.get("template").ok_or_else(|| {
+            ShellError::labeled_error(
+                "No file or directory specified",
+                "for command",
+                Tag::default(),
+            )
+        })?;
+
+        let num_rows = args
+            .get("num-rows")
+            .map(|v| v.as_u64().unwrap())
+            .unwrap_or(1);
+        let path = path.as_path().unwrap();
+        let template = fs::read_to_string(path).unwrap();
+
+        for _ in 0..num_rows {
+            let generated = tera.render_str(&template, &ctx).unwrap();
+            let content = serde_json::from_str(&generated).unwrap();
+            let content_converted = convert_json_value_to_nu_value(&content, Tag::default());
+            results.push(content_converted);
+        }
+    }
 
     Ok(OutputStream::from(results))
 }
 
-use fake::faker::internet::raw::*;
-use fake::faker::name::raw::*;
-use fake::locales::*;
-use fake::Fake;
+static LIST_FUNCTIONS: &str = r#"[
+    { "name": "uuid()", "description": "UUID v4", "example": "{{ uuid() }}" },
+    { "name": "firstName()", "description": "First name", "example": "{{ firstName() }}" },
+    { "name": "lastName()", "description": "Last name", "example": "{{ lastName() }}" },
+    { "name": "name()", "description": "firstName and lastName combined", "example": "{{ name() }}" },
+    { "name": "title()", "description": "Person title", "example": "{{ title() }}" },
+    { "name": "title()", "description": "Person title", "example": "{{ title() }}" },
+    { "name": "nameWithTitle()", "description": "name and title combined", "example": "{{ nameWithTitle() }}" },
+    { "name": "userName()", "description": "Username", "example": "{{ userName() }}" }
+]"#;
 
 fn fake_name(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
     let data: String = Name(EN).fake();
@@ -112,5 +157,10 @@ fn fake_title(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value>
 
 fn fake_username(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
     let data: String = Username(EN).fake();
+    Ok(tera::Value::from(data))
+}
+
+fn fake_uuid(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
+    let data: String = format!("{}", uuid::Uuid::new_v4());
     Ok(tera::Value::from(data))
 }
