41bd09dc1df6f8fc3f41d8b7c3e338715e0130c1
Michael Nitschinger <michael@nitschinger.at>
2020-03-15 18:16:01 +0100

    More fake functions

--- src/cli/fake_data/mod.rs
+++ src/cli/fake_data/mod.rs
@@ -9,10 +9,12 @@ use nu_cli::{CommandArgs, CommandRegistry, OutputStream};
 use nu_errors::ShellError;
 use nu_protocol::{Signature, SyntaxShape};
 use nu_source::Tag;
+use serde_json::{from_value, Value};
 use std::collections::HashMap;
 use std::fs;
 use std::sync::Arc;
 use tera::{Context, Tera};
+use uuid::Uuid;
 
 pub struct FakeData {
     state: Arc<State>,
@@ -70,13 +72,7 @@ async fn run_fake(
     let ctx = Context::new();
     let mut tera = Tera::default();
 
-    tera.register_function("uuid", fake_uuid);
-    tera.register_function("name", fake_name);
-    tera.register_function("firstName", fake_first_name);
-    tera.register_function("lastName", fake_last_name);
-    tera.register_function("title", fake_title);
-    tera.register_function("nameWithTitle", fake_name_with_title);
-    tera.register_function("userName", fake_username);
+    register_functions(&mut tera);
 
     let mut results = Vec::new();
 
@@ -119,48 +115,93 @@ async fn run_fake(
     Ok(OutputStream::from(results))
 }
 
+fn register_functions(tera: &mut Tera) {
+    // Group "misc"
+    tera.register_function("uuid", |_: &HashMap<String, Value>| {
+        Ok(Value::from(format!("{}", Uuid::new_v4())))
+    });
+
+    // Group "name"
+    tera.register_function("name", |_: &HashMap<String, Value>| {
+        Ok(Value::from(Name(EN).fake::<String>()))
+    });
+    tera.register_function("firstName", |_: &HashMap<String, Value>| {
+        Ok(Value::from(FirstName(EN).fake::<String>()))
+    });
+    tera.register_function("lastName", |_: &HashMap<String, Value>| {
+        Ok(Value::from(LastName(EN).fake::<String>()))
+    });
+    tera.register_function("title", |_: &HashMap<String, Value>| {
+        Ok(Value::from(Title(EN).fake::<String>()))
+    });
+    tera.register_function("nameWithTitle", |_: &HashMap<String, Value>| {
+        Ok(Value::from(NameWithTitle(EN).fake::<String>()))
+    });
+    tera.register_function("suffix", |_: &HashMap<String, Value>| {
+        Ok(Value::from(Suffix(EN).fake::<String>()))
+    });
+
+    // Group "internet"
+    tera.register_function("color", |_: &HashMap<String, Value>| {
+        Ok(Value::from(Color(EN).fake::<String>()))
+    });
+    tera.register_function("domainSuffix", |_: &HashMap<String, Value>| {
+        Ok(Value::from(DomainSuffix(EN).fake::<String>()))
+    });
+    tera.register_function("freeEmail", |_: &HashMap<String, Value>| {
+        Ok(Value::from(FreeEmail(EN).fake::<String>()))
+    });
+    tera.register_function("freeEmailProvider", |_: &HashMap<String, Value>| {
+        Ok(Value::from(FreeEmailProvider(EN).fake::<String>()))
+    });
+    tera.register_function("ipV4", |_: &HashMap<String, Value>| {
+        Ok(Value::from(IPv4(EN).fake::<String>()))
+    });
+    tera.register_function("ipV6", |_: &HashMap<String, Value>| {
+        Ok(Value::from(IPv6(EN).fake::<String>()))
+    });
+   tera.register_function("password", |args: &HashMap<String, Value>| {
+    let length = match args.get("length") {
+        Some(val) => match from_value::<usize>(val.clone()) {
+            Ok(v) => v,
+            Err(_) => {
+                return Err(tera::Error::msg(format!(
+                    "Function `password` received length={} but `length` can only be a number",
+                    val
+                )));
+            }
+        },
+        None => 10,
+    };
+        Ok(Value::from(Password(EN, length..length+1).fake::<String>()))
+   });
+    tera.register_function("safeEmail", |_: &HashMap<String, Value>| {
+        Ok(Value::from(SafeEmail(EN).fake::<String>()))
+    });
+    tera.register_function("userAgent", |_: &HashMap<String, Value>| {
+        Ok(Value::from(UserAgent(EN).fake::<String>()))
+    });
+    tera.register_function("userName", |_: &HashMap<String, Value>| {
+        Ok(Value::from(Username(EN).fake::<String>()))
+    });
+}
+
 static LIST_FUNCTIONS: &str = r#"[
-    { "name": "uuid()", "description": "UUID v4", "example": "{{ uuid() }}" },
-    { "name": "firstName()", "description": "First name", "example": "{{ firstName() }}" },
-    { "name": "lastName()", "description": "Last name", "example": "{{ lastName() }}" },
-    { "name": "name()", "description": "firstName and lastName combined", "example": "{{ name() }}" },
-    { "name": "title()", "description": "Person title", "example": "{{ title() }}" },
-    { "name": "title()", "description": "Person title", "example": "{{ title() }}" },
-    { "name": "nameWithTitle()", "description": "name and title combined", "example": "{{ nameWithTitle() }}" },
-    { "name": "userName()", "description": "Username", "example": "{{ userName() }}" }
+    { "group": "misc", "name": "uuid()", "description": "UUID v4", "example": "{{ uuid() }}" },
+    { "group": "name", "name": "firstName()", "description": "First name", "example": "{{ firstName() }}" },
+    { "group": "name", "name": "lastName()", "description": "Last name", "example": "{{ lastName() }}" },
+    { "group": "name", "name": "name()", "description": "firstName and lastName combined", "example": "{{ name() }}" },
+    { "group": "name", "name": "title()", "description": "Person title", "example": "{{ title() }}" },
+    { "group": "name", "name": "nameWithTitle()", "description": "name and title combined", "example": "{{ nameWithTitle() }}" },    
+    { "group": "name", "name": "suffix()", "description": "Person info/degree", "example": "{{ suffix() }}" },
+    { "group": "internet", "name": "color()", "description": "Color hex code", "example": "{{ color() }}" },
+    { "group": "internet", "name": "domainSuffix()", "description": "Domain suffix", "example": "{{ domainSuffix() }}" },
+    { "group": "internet", "name": "freeEmail()", "description": "Email that might exist", "example": "{{ freeEmail() }}" },
+    { "group": "internet", "name": "freeEmailProvider()", "description": "Provider that exists", "example": "{{ freeEmailProvider() }}" },
+    { "group": "internet", "name": "ipV4()", "description": "IP v4 address", "example": "{{ ipV4() }}" },
+    { "group": "internet", "name": "ipV6()", "description": "IP v6 address", "example": "{{ ipV6() }}" },
+    { "group": "internet", "name": "userAgent()", "description": "User Agent", "example": "{{ userAgent() }}" },
+    { "group": "internet", "name": "safeEmail()", "description": "Email that does not exist", "example": "{{ safeEmail() }}" },
+    { "group": "internet", "name": "userName()", "description": "Username", "example": "{{ userName() }}" },
+    { "group": "internet", "name": "password(length=10)", "description": "Password", "example": "{{ password() }}" }
 ]"#;
-
-fn fake_name(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
-    let data: String = Name(EN).fake();
-    Ok(tera::Value::from(data))
-}
-
-fn fake_name_with_title(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
-    let data: String = NameWithTitle(EN).fake();
-    Ok(tera::Value::from(data))
-}
-
-fn fake_first_name(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
-    let data: String = FirstName(EN).fake();
-    Ok(tera::Value::from(data))
-}
-
-fn fake_last_name(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
-    let data: String = LastName(EN).fake();
-    Ok(tera::Value::from(data))
-}
-
-fn fake_title(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
-    let data: String = Title(EN).fake();
-    Ok(tera::Value::from(data))
-}
-
-fn fake_username(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
-    let data: String = Username(EN).fake();
-    Ok(tera::Value::from(data))
-}
-
-fn fake_uuid(_args: &HashMap<String, tera::Value>) -> tera::Result<tera::Value> {
-    let data: String = format!("{}", uuid::Uuid::new_v4());
-    Ok(tera::Value::from(data))
-}
