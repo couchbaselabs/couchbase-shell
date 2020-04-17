efbb94c4ad75f94423e5e4498f0852fd1bb26912
Charles Dixon <chvckd@gmail.com>
2020-04-17 12:27:46 +0100

    Update upsert to encode values as JSON

--- src/cli/util.rs
+++ src/cli/util.rs
@@ -1,4 +1,6 @@
-use nu_protocol::{Primitive, TaggedDictBuilder, UntaggedValue, Value};
+use nu_cli::ToPrimitive;
+use nu_errors::ShellError;
+use nu_protocol::{Primitive, TaggedDictBuilder, UnspannedPathMember, UntaggedValue, Value};
 use nu_source::Tag;
 
 pub fn convert_json_value_to_nu_value(v: &serde_json::Value, tag: impl Into<Tag>) -> Value {
@@ -32,4 +34,103 @@ pub fn convert_json_value_to_nu_value(v: &serde_json::Value, tag: impl Into<Tag>
             collected.into_value()
         }
     }
+}
+
+// Adapted from https://github.com/nushell/nushell/blob/master/crates/nu-cli/src/commands/to_json.rs
+pub fn convert_nu_value_to_json_value(v: &Value) -> Result<serde_json::Value, ShellError> {
+    Ok(match &v.value {
+        UntaggedValue::Primitive(Primitive::Boolean(b)) => serde_json::Value::Bool(*b),
+        UntaggedValue::Primitive(Primitive::Bytes(b)) => {
+            serde_json::Value::Number(serde_json::Number::from(*b))
+        }
+        UntaggedValue::Primitive(Primitive::Duration(secs)) => {
+            serde_json::Value::Number(serde_json::Number::from(*secs))
+        }
+        UntaggedValue::Primitive(Primitive::Date(d)) => serde_json::Value::String(d.to_string()),
+        UntaggedValue::Primitive(Primitive::EndOfStream) => serde_json::Value::Null,
+        UntaggedValue::Primitive(Primitive::BeginningOfStream) => serde_json::Value::Null,
+        UntaggedValue::Primitive(Primitive::Decimal(f)) => {
+            if let Some(f) = f.to_f32() {
+                if let Some(num) = serde_json::Number::from_f64(
+                    f.to_f64().expect("TODO: What about really big decimals?"),
+                ) {
+                    serde_json::Value::Number(num)
+                } else {
+                    return Err(ShellError::labeled_error(
+                        "Could not convert value to decimal number",
+                        "could not convert to decimal",
+                        &v.tag,
+                    ));
+                }
+            } else {
+                return Err(ShellError::labeled_error(
+                    "Could not convert value to decimal number",
+                    "could not convert to decimal",
+                    &v.tag,
+                ));
+            }
+        }
+
+        UntaggedValue::Primitive(Primitive::Int(i)) => {
+            serde_json::Value::Number(serde_json::Number::from(i.to_i64().unwrap()))
+        }
+        UntaggedValue::Primitive(Primitive::Nothing) => serde_json::Value::Null,
+        UntaggedValue::Primitive(Primitive::Pattern(s)) => serde_json::Value::String(s.clone()),
+        UntaggedValue::Primitive(Primitive::String(s)) => serde_json::Value::String(s.clone()),
+        UntaggedValue::Primitive(Primitive::Line(s)) => serde_json::Value::String(s.clone()),
+        UntaggedValue::Primitive(Primitive::ColumnPath(path)) => serde_json::Value::Array(
+            path.iter()
+                .map(|x| match &x.unspanned {
+                    UnspannedPathMember::String(string) => {
+                        Ok(serde_json::Value::String(string.clone()))
+                    }
+                    UnspannedPathMember::Int(int) => Ok(serde_json::Value::Number(
+                        serde_json::Number::from(int.to_i64().unwrap()),
+                    )),
+                })
+                .collect::<Result<Vec<serde_json::Value>, ShellError>>()?,
+        ),
+        UntaggedValue::Primitive(Primitive::Path(s)) => {
+            serde_json::Value::String(s.display().to_string())
+        }
+
+        UntaggedValue::Table(l) => serde_json::Value::Array(json_list(l)?),
+        UntaggedValue::Error(e) => return Err(e.clone()),
+        UntaggedValue::Block(_) | UntaggedValue::Primitive(Primitive::Range(_)) => {
+            serde_json::Value::Null
+        }
+        UntaggedValue::Primitive(Primitive::Binary(b)) => serde_json::Value::Array(
+            b.iter()
+                .map(|x| {
+                    serde_json::Number::from_f64(*x as f64).ok_or_else(|| {
+                        ShellError::labeled_error(
+                            "Can not convert number from floating point",
+                            "can not convert to number",
+                            &v.tag,
+                        )
+                    })
+                })
+                .collect::<Result<Vec<serde_json::Number>, ShellError>>()?
+                .into_iter()
+                .map(serde_json::Value::Number)
+                .collect(),
+        ),
+        UntaggedValue::Row(o) => {
+            let mut m = serde_json::Map::new();
+            for (k, v) in o.entries.iter() {
+                m.insert(k.clone(), convert_nu_value_to_json_value(v)?);
+            }
+            serde_json::Value::Object(m)
+        }
+    })
+}
+
+fn json_list(input: &[Value]) -> Result<Vec<serde_json::Value>, ShellError> {
+    let mut out = vec![];
+
+    for value in input {
+        out.push(convert_nu_value_to_json_value(value)?);
+    }
+
+    Ok(out)
 }
