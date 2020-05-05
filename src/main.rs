f610cae9673ca5269a2996f02170c8e3008cea89
Michael Nitschinger <michael@nitschinger.at>
2020-05-05 19:34:34 +0200

    Refactor UI into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod cli;
 mod config;
 mod state;
+mod ui;
 
 use crate::cli::*;
+use crate::ui::*;
 use crate::config::ShellConfig;
 use crate::state::RemoteCluster;
-use http::Uri;
 use log::{debug, warn};
-use rust_embed::RustEmbed;
 use state::State;
 use std::collections::HashMap;
 use std::error::Error;
@@ -15,11 +15,6 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::sync::Arc;
 use structopt::StructOpt;
-use warp::{http::header::HeaderValue, path::Tail, reply::Response, Filter, Rejection, Reply};
-
-#[derive(RustEmbed)]
-#[folder = "ui-assets/"]
-struct Asset;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -67,25 +62,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let state = Arc::new(State::new(clusters, active));
 
     if opt.ui {
-        let web_state = state.clone();
-        tokio::task::spawn(async {
-            let index = warp::path::end().and_then(serve_index);
-
-            let p_state = web_state.clone();
-            let pools = warp::path("pools")
-                .and(warp::any().map(move || p_state.clone()))
-                .and_then(serve_pools);
-            let serve_query_service = warp::post()
-                .and(warp::path!("_p" / "query" / "query" / "service"))
-                .and(warp::any().map(move || web_state.clone()))
-                .and(warp::body::json())
-                .and_then(serve_query_service);
-
-            let ui_assets = warp::path("ui").and(warp::path::tail()).and_then(serve);
-
-            let routes = index.or(pools).or(serve_query_service).or(ui_assets);
-            warp::serve(routes).run(([127, 0, 0, 1], 1908)).await;
-        });
+        tokio::task::spawn(spawn_and_serve(state.clone()));
     }
 
     let mut syncer = nu_cli::EnvironmentSyncer::new();
@@ -134,74 +111,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     nu_cli::cli(Some(syncer), Some(context)).await
-}
-
-async fn serve_index() -> Result<impl Reply, Rejection> {
-    Ok(warp::redirect(Uri::from_static("/ui/index.html")))
-}
-
-async fn serve_pools(state: Arc<State>) -> Result<impl Reply, Rejection> {
-    let client = reqwest::Client::new();
-
-    let host = state.active_cluster().connstr().replace("couchbase://", "");
-    let uri = format!("http://{}:8091/pools", host);
-
-    let resp = client
-        .get(&uri)
-        .basic_auth(
-            state.active_cluster().username(),
-            Some(state.active_cluster().password()),
-        )
-        .send()
-        .await
-        .unwrap()
-        .json::<serde_json::Value>()
-        .await
-        .unwrap();
-
-    Ok(warp::reply::json(&resp))
-}
-
-async fn serve_query_service(
-    state: Arc<State>,
-    body: serde_json::Value,
-) -> Result<impl Reply, Rejection> {
-    let client = reqwest::Client::new();
-
-    let host = state.active_cluster().connstr().replace("couchbase://", "");
-    let uri = format!("http://{}:8091/_p/query/query/service", host);
-
-    let resp = client
-        .post(&uri)
-        .basic_auth(
-            state.active_cluster().username(),
-            Some(state.active_cluster().password()),
-        )
-        .json(&body)
-        .send()
-        .await
-        .unwrap()
-        .json::<serde_json::Value>()
-        .await
-        .unwrap();
-
-    Ok(warp::reply::json(&resp))
-}
-
-async fn serve(path: Tail) -> Result<impl Reply, Rejection> {
-    serve_impl(path.as_str())
-}
-
-fn serve_impl(path: &str) -> Result<impl Reply, Rejection> {
-    let asset = Asset::get(path).ok_or_else(warp::reject::not_found)?;
-    let mime = mime_guess::from_path(path).first_or_octet_stream();
-
-    let mut res = Response::new(asset.into());
-    res.headers_mut().insert(
-        "content-type",
-        HeaderValue::from_str(mime.as_ref()).unwrap(),
-    );
-    Ok(res)
 }
 
 #[derive(Debug, StructOpt)]
