e0a78e7f8a9f09f3988545e36d04899ce9e294e2
Michael Nitschinger <michael@nitschinger.at>
2020-10-12 15:19:33 +0200

    Modify css for docs

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-asciidoctor -a toc=left -a toclevels=4 -a source-highlighter=coderay index.adoc
-
+asciidoctor -a toc=left -a toclevels=4 -a source-highlighter=coderay -a stylesheet=couchbase.css -a linkcss=true index.adoc
