6fe512a49149018b57814418759f85116d8c459b
Charles Dixon <chvckd@gmail.com>
2022-06-21 14:20:55 +0100

    Automatically detect query context

--- src/cli/query_advise.rs
+++ src/cli/query_advise.rs
@@ -1,16 +1,9 @@
-use crate::cli::util::{
-    cluster_identifiers_from, cluster_not_found_error, convert_json_value_to_nu_value,
-    convert_row_to_nu_value, duration_to_golang_string, generic_unspanned_error,
-    map_serde_deserialize_error_to_shell_error,
-};
-use crate::client::QueryRequest;
+use crate::cli::util::{cluster_identifiers_from, get_active_cluster};
 use crate::state::State;
 use log::debug;
-use std::collections::HashMap;
-use std::ops::Add;
 use std::sync::{Arc, Mutex};
-use tokio::time::Instant;
 
+use crate::cli::query::{handle_query_response, query_context_from_args, send_query};
 use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
@@ -38,6 +31,7 @@ impl Command for QueryAdvise {
         Signature::build("query advise")
             .required("statement", SyntaxShape::String, "the query statement")
             .switch("with-meta", "Includes related metadata in the result", None)
+            .switch("disable-context", "disable automatically detecting the query context based on the active bucket and scope", None)
             .named(
                 "clusters",
                 SyntaxShape::String,
@@ -72,71 +66,34 @@ fn run(
     let span = call.head;
     let ctrl_c = engine_state.ctrlc.as_ref().unwrap().clone();
 
-    let with_meta = call.has_flag("with-meta");
-
     let statement: String = call.req(engine_state, stack, 0)?;
     let statement = format!("ADVISE {}", statement);
 
     let cluster_identifiers = cluster_identifiers_from(engine_state, stack, &state, call, true)?;
-    let guard = state.lock().unwrap();
-    debug!("Running n1ql query {}", &statement);
 
     let mut results: Vec<Value> = vec![];
     for identifier in cluster_identifiers {
-        let active_cluster = match guard.clusters().get(&identifier) {
-            Some(c) => c,
-            None => {
-                return Err(cluster_not_found_error(identifier, call.span()));
-            }
-        };
-        let response = active_cluster.cluster().http_client().query_request(
-            QueryRequest::Execute {
-                statement: statement.clone(),
-                scope: None,
-                timeout: duration_to_golang_string(active_cluster.timeouts().query_timeout()),
-            },
-            Instant::now().add(active_cluster.timeouts().query_timeout()),
+        let guard = state.lock().unwrap();
+        let active_cluster = get_active_cluster(identifier.clone(), &guard, span.clone())?;
+
+        let maybe_scope = query_context_from_args(active_cluster, engine_state, stack, call)?;
+
+        debug!("Running n1ql advise query {}", &statement);
+
+        let response = send_query(
+            active_cluster,
+            statement.clone(),
+            maybe_scope,
             ctrl_c.clone(),
         )?;
+        drop(guard);
 
-        if with_meta {
-            let content: serde_json::Value = serde_json::from_str(response.content())
-                .map_err(map_serde_deserialize_error_to_shell_error)?;
-            results.push(convert_row_to_nu_value(&content, span, identifier.clone())?);
-        } else {
-            let content: HashMap<String, serde_json::Value> =
-                serde_json::from_str(response.content())
-                    .map_err(map_serde_deserialize_error_to_shell_error)?;
-            if let Some(content_errors) = content.get("errors") {
-                if let Some(arr) = content_errors.as_array() {
-                    for result in arr {
-                        results.push(convert_row_to_nu_value(result, span, identifier.clone())?);
-                    }
-                } else {
-                    return Err(generic_unspanned_error(
-                        "Query errors not an array - malformed response",
-                        format!("Query errors not an array - {}", content_errors.to_string(),),
-                    ));
-                }
-            } else if let Some(content_results) = content.get("results") {
-                if let Some(arr) = content_results.as_array() {
-                    for result in arr {
-                        results.push(convert_json_value_to_nu_value(result, span).unwrap());
-                    }
-                } else {
-                    return Err(generic_unspanned_error(
-                        "Query results not an array - malformed response",
-                        format!(
-                            "Query results not an array - {}",
-                            content_results.to_string(),
-                        ),
-                    ));
-                }
-            } else {
-                // Queries like "create index" can end up here.
-                continue;
-            };
-        }
+        results.extend(handle_query_response(
+            call.has_flag("with-meta"),
+            identifier.clone(),
+            response,
+            span.clone(),
+        )?);
     }
 
     Ok(Value::List {
