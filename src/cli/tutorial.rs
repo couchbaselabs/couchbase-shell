ecf3ded4b521be821f015581a55279ddb988db73
Charles Dixon <chvckd@gmail.com>
2022-10-03 13:12:41 +0100

    Update tutorial to be more general

--- src/cli/tutorial.rs
+++ src/cli/tutorial.rs
@@ -1,8 +1,5 @@
-use crate::client::ManagementRequest;
 use crate::state::State;
-use std::ops::Add;
 use std::sync::{Arc, Mutex};
-use tokio::time::Instant;
 
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
@@ -45,40 +42,16 @@ impl Command for Tutorial {
 
 fn run_tutorial(
     state: Arc<Mutex<State>>,
-    engine_state: &EngineState,
+    _engine_state: &EngineState,
     _stack: &mut Stack,
     call: &Call,
     _input: PipelineData,
 ) -> Result<PipelineData, ShellError> {
-    let ctrl_c = engine_state.ctrlc.as_ref().unwrap().clone();
-
     let guard = state.lock().unwrap();
     let tutorial = guard.tutorial();
-    let exists = match guard.active_cluster() {
-        Some(active_cluster) => {
-            if active_cluster.capella_org().is_none() {
-                let resp = active_cluster.cluster().http_client().management_request(
-                    ManagementRequest::GetBucket {
-                        name: "travel-sample".to_string(),
-                    },
-                    Instant::now().add(active_cluster.timeouts().management_timeout()),
-                    ctrl_c,
-                );
-
-                match resp {
-                    Ok(r) => matches!(r.status(), 200),
-                    Err(_) => false,
-                }
-            } else {
-                // Bit of a hack, if the user is on cloud then they can't enable travel-sample
-                true
-            }
-        }
-        None => true,
-    };
 
     Ok(Value::String {
-        val: tutorial.current_step(exists),
+        val: tutorial.current_step(),
         span: call.head,
     }
     .into_pipeline_data())
