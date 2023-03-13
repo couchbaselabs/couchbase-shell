125d7a473f294594241a5e2eca41315780bfb0d8
Charles Dixon <chvckd@gmail.com>
2023-03-13 09:43:01 +0000

    Improve debuggability of tests

--- tests/integration/support/macros.rs
+++ tests/integration/support/macros.rs
@@ -22,6 +22,8 @@ macro_rules! cbsh {
         pub use $crate::support::NATIVE_PATH_ENV_VAR;
         pub use std::time::Instant;
         pub use std::ops::Sub;
+        use std::env;
+        use $crate::support::LOGGER_PREFIX;
 
         let test_bins = $crate::support::fs::binaries();
 
@@ -50,11 +52,13 @@ macro_rules! cbsh {
         let process = match Command::new($crate::support::fs::executable_path())
             .env("PWD", &target_cwd)  // setting PWD is enough to set cwd
             .env(NATIVE_PATH_ENV_VAR, paths_joined)
+            .envs(env::vars())  // passthrough env vars
             .current_dir(&target_cwd)
             // .arg("--no-history")
             // .arg("--config-file")
             // .arg($crate::support::fs::DisplayPath::display_path(&$crate::support::fs::fixtures().join("playground/config/default.toml")))
-            .arg("--silent")
+            .arg("--logger-prefix")
+            .arg(format!("\"{}\"", LOGGER_PREFIX.to_string()))
             .arg("-c")
             .arg(format!("{}", $crate::support::fs::DisplayPath::display_path(&path)))
             .stdout(Stdio::piped())
@@ -79,91 +83,22 @@ macro_rules! cbsh {
         let err = String::from_utf8_lossy(&output.stderr);
         let taken = Instant::now().sub(start);
 
-        println!("=== cmd\n{:?}", path);
+        println!("\n=== cmd\n{:?}", path);
         println!("Took: {:?}\n", taken);
         println!("=== stdout\n{}", out);
-        println!("=== stderr\n{}", err);
+        println!("=== stderr\n");
 
-        $crate::support::Outcome::new(out,err.into_owned())
-    }};
-}
+        let lines = err.split('\n');
+        let mut actual_err = Vec::new();
+        for line in lines {
+            if line.starts_with(LOGGER_PREFIX) {
+                println!("{}\n", line.strip_prefix(LOGGER_PREFIX).unwrap());
+            } else {
+                actual_err.push(line);
+            }
+        }
 
-#[macro_export]
-macro_rules! cbsh_with_plugins {
-    (cwd: $cwd:expr, $path:expr, $($part:expr),*) => {{
-        use $crate::support::fs::DisplayPath;
-
-        let path = format!($path, $(
-            $part.display_path()
-        ),*);
-
-        cbsh_with_plugins!($cwd, &path)
-    }};
-
-    (cwd: $cwd:expr, $path:expr) => {{
-        cbsh_with_plugins!($cwd, $path)
-    }};
-
-    ($cwd:expr, $path:expr) => {{
-        pub use std::error::Error;
-        pub use std::io::prelude::*;
-        pub use std::process::{Command, Stdio};
-        pub use crate::support::NATIVE_PATH_ENV_VAR;
-
-        let commands = &*format!(
-            "
-                            {}
-                            exit",
-            $crate::support::fs::DisplayPath::display_path(&$path)
-        );
-
-        let test_bins = $crate::support::fs::binaries();
-        let test_bins = nu_path::canonicalize(&test_bins).unwrap_or_else(|e| {
-            panic!(
-                "Couldn't canonicalize dummy binaries path {}: {:?}",
-                test_bins.display(),
-                e
-            )
-        });
-
-        let mut paths = $crate::support::shell_os_paths();
-        paths.insert(0, test_bins);
-
-        let paths_joined = match std::env::join_paths(paths) {
-            Ok(all) => all,
-            Err(_) => panic!("Couldn't join paths for PATH var."),
-        };
-
-        let target_cwd = $crate::support::fs::in_directory(&$cwd);
-
-        let mut process = match Command::new($crate::support::fs::executable_path())
-            .env("PWD", &target_cwd)  // setting PWD is enough to set cwd
-            .env(NATIVE_PATH_ENV_VAR, paths_joined)
-            .arg("--silent")
-            .stdout(Stdio::piped())
-            .stdin(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()
-        {
-            Ok(child) => child,
-            Err(why) => panic!("Can't run test {}", why.to_string()),
-        };
-
-        let stdin = process.stdin.as_mut().expect("couldn't open stdin");
-        stdin
-            .write_all(commands.as_bytes())
-            .expect("couldn't write to stdin");
-
-        let output = process
-            .wait_with_output()
-            .expect("couldn't read from stdout/stderr");
-
-        let out = $crate::support::macros::read_std(&output.stdout);
-        let err = String::from_utf8_lossy(&output.stderr);
-
-            println!("=== stderr\n{}", err);
-
-        $crate::support::Outcome::new(out,err.into_owned())
+        $crate::support::Outcome::new(out, actual_err.join("\n"))
     }};
 }
 
