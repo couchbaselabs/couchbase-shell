4a27eddd3f0a10d732ca39462c338177e4a7810a
Jack Westwood <jack.westwood@couchbase.com>
2024-07-08 11:04:06 +0100

    Show active capella org in cbenv output when set

--- src/cli/cbenv_cmd.rs
+++ src/cli/cbenv_cmd.rs
@@ -25,11 +25,6 @@ impl Command for UseCmd {
     fn signature(&self) -> Signature {
         Signature::build("cb-env")
             .switch(
-                "capella",
-                "show default execution environment of capella",
-                None,
-            )
-            .switch(
                 "timeouts",
                 "show default execution environment for timeouts",
                 None,
@@ -59,108 +54,101 @@ fn use_cmd(
     call: &Call,
     _input: PipelineData,
 ) -> Result<PipelineData, ShellError> {
-    let show_capella = call.has_flag(engine_state, stack, "capella")?;
     let show_timeouts = call.has_flag(engine_state, stack, "timeouts")?;
 
     let span = call.head;
 
     let guard = state.lock().unwrap();
     let mut results = NuValueMap::default();
-    if show_capella {
-        let org = guard.active_capella_org()?;
 
-        results.add_string(
-            "capella-organisation",
-            guard
-                .active_capella_org_name()
-                .unwrap_or_else(|| String::from("")),
-            span,
-        );
-        results.add_string(
-            "project",
-            org.active_project().unwrap_or_else(|| String::from("")),
-            span,
-        );
-        if show_timeouts {
-            results.add_i64(
-                "management-timeout (ms)",
-                org.timeout().as_millis() as i64,
+    match guard.active_cluster() {
+        Some(active) => {
+            let display_name = if let Some(dn) = active.display_name() {
+                dn
+            } else {
+                active.username().to_string()
+            };
+            results.add_string("username", active.username(), span);
+            results.add_string("display_name", display_name, span);
+            results.add_string("cluster", guard.active(), span);
+            results.add_string(
+                "bucket",
+                active.active_bucket().unwrap_or_else(|| String::from("")),
                 span,
             );
-        }
-    } else {
-        match guard.active_cluster() {
-            Some(active) => {
-                let display_name = if let Some(dn) = active.display_name() {
-                    dn
-                } else {
-                    active.username().to_string()
-                };
-                results.add_string("username", active.username(), span);
-                results.add_string("display_name", display_name, span);
-                results.add_string("cluster", guard.active(), span);
-                results.add_string(
-                    "bucket",
-                    active.active_bucket().unwrap_or_else(|| String::from("")),
-                    span,
-                );
-                results.add_string(
-                    "scope",
-                    active
-                        .active_scope()
-                        .unwrap_or_else(|| String::from("_default")),
-                    span,
-                );
-                results.add_string(
-                    "collection",
-                    active
-                        .active_collection()
-                        .unwrap_or_else(|| String::from("_default")),
-                    span,
-                );
-                results.add_string("cluster_type", active.cluster_type(), span);
-                if let Some(co) = active.capella_org() {
-                    results.add_string("capella-organization", co, span);
-                }
 
-                if show_timeouts {
-                    let timeouts = active.timeouts();
-                    results.add_i64(
-                        "data-timeout (ms)",
-                        timeouts.data_timeout().as_millis() as i64,
-                        span,
-                    );
-                    results.add_i64(
-                        "management-timeout (ms)",
-                        timeouts.management_timeout().as_millis() as i64,
-                        span,
-                    );
-                    results.add_i64(
-                        "analytics-timeout (ms)",
-                        timeouts.analytics_timeout().as_millis() as i64,
-                        span,
-                    );
-                    results.add_i64(
-                        "query-timeout (ms)",
-                        timeouts.query_timeout().as_millis() as i64,
-                        span,
-                    );
-                    results.add_i64(
-                        "search-timeout (ms)",
-                        timeouts.search_timeout().as_millis() as i64,
-                        span,
-                    );
-                }
+            results.add_string(
+                "scope",
+                active.active_scope().unwrap_or_else(|| String::from("")),
+                span,
+            );
+            results.add_string(
+                "collection",
+                active
+                    .active_collection()
+                    .unwrap_or_else(|| String::from("")),
+                span,
+            );
+            results.add_string("cluster_type", active.cluster_type(), span);
+
+            results.add_string(
+                "cluster-organization",
+                active.capella_org().unwrap_or(String::from("")),
+                span,
+            );
+
+            if show_timeouts {
+                let timeouts = active.timeouts();
+                results.add_i64(
+                    "data-timeout (ms)",
+                    timeouts.data_timeout().as_millis() as i64,
+                    span,
+                );
+                results.add_i64(
+                    "management-timeout (ms)",
+                    timeouts.management_timeout().as_millis() as i64,
+                    span,
+                );
+                results.add_i64(
+                    "analytics-timeout (ms)",
+                    timeouts.analytics_timeout().as_millis() as i64,
+                    span,
+                );
+                results.add_i64(
+                    "query-timeout (ms)",
+                    timeouts.query_timeout().as_millis() as i64,
+                    span,
+                );
+                results.add_i64(
+                    "search-timeout (ms)",
+                    timeouts.search_timeout().as_millis() as i64,
+                    span,
+                );
             }
-            None => {
-                results.add_string("username", String::from(""), span);
-                results.add_string("display_name", String::from(""), span);
-                results.add_string("cluster", String::from(""), span);
-                results.add_string("bucket", String::from(""), span);
-                results.add_string("scope", String::from(""), span);
-                results.add_string("collection", String::from(""), span);
-                results.add_string("cluster_type", String::from(""), span);
-            }
+        }
+        None => {
+            results.add_string("username", String::from(""), span);
+            results.add_string("display_name", String::from(""), span);
+            results.add_string("cluster", String::from(""), span);
+            results.add_string("bucket", String::from(""), span);
+            results.add_string("scope", String::from(""), span);
+            results.add_string("collection", String::from(""), span);
+            results.add_string("cluster_type", String::from(""), span);
+        }
+    }
+
+    if let Some(active_org) = guard.active_capella_org_name() {
+        results.add_string("active-organization", active_org, span);
+        let (project, timeout) = match guard.active_capella_org() {
+            Ok(org) => (
+                org.active_project().unwrap_or_else(|| String::from("")),
+                org.timeout().as_millis() as i64,
+            ),
+            Err(_) => ("".to_string(), 0),
+        };
+        results.add_string("project", project, span);
+        if show_timeouts {
+            results.add_i64("management-timeout (ms)", timeout, span);
         }
     }
 
