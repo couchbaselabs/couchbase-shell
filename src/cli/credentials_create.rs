1158ceb2dcd754bdc7353b6870d789e3038a25a5
Jack Westwood <jack.westwood@couchbase.com>
2024-07-25 09:04:31 +0100

    Add command to create credentials on Capella cluster

--- src/cli/credentials_create.rs
+++ src/cli/credentials_create.rs
@@ -31,7 +31,7 @@ impl Command for crate::cli::CredentialsCreate {
     }
 
     fn usage(&self) -> &str {
-        "Creates credentials on a Capella cluster"
+        "Creates a new cluster on the active Capella organization"
     }
 
     fn run(
@@ -94,7 +94,7 @@ fn credentials_create(
             project_id.clone(),
         )?;
 
-        if json_cluster.state() != "healthy" {
+        if json_cluster.state() != "healthy".to_string() {
             return Err(ShellError::GenericError {
                 error: "Cannot create credentials until cluster state is healthy".to_string(),
                 msg: "".to_string(),
@@ -131,7 +131,7 @@ fn credentials_create(
             }
         };
 
-        let payload = CredentialsCreateRequest::new(name.clone(), password.clone());
+        let payload = CredentialsCreateRequest::new(name.clone(), password.clone().into());
 
         client
             .create_credentials(
