c8540c2f339ae4d472655195fd257933911cfa1a
Jack Westwood <jack.westwood@couchbase.com>
2025-01-06 14:19:30 +0000

    Add test for scopes commands

---
+++ tests/scopes.rs
@@ -0,0 +1,23 @@
+use crate::common::playground::CBPlayground;
+use nu_test_support::pipeline;
+
+mod common;
+
+#[test]
+fn scope_management() {
+    CBPlayground::setup("scope_management", None, None, |dirs, _| {
+        let out = cbsh!(cwd: dirs.test(), pipeline(r#"scopes create test_scope"#));
+        assert_eq!("", out.err);
+
+        let out = cbsh!(cwd: dirs.test(), pipeline(r#""test_scope" in (scopes | get scope)"#));
+        assert_eq!("", out.err);
+        assert_eq!("true", out.out);
+
+        let out = cbsh!(cwd: dirs.test(), pipeline(r#"scopes drop test_scope"#));
+        assert_eq!("", out.err);
+
+        let out = cbsh!(cwd: dirs.test(), pipeline(r#""test_scope" not-in (scopes | get scope)"#));
+        assert_eq!("", out.err);
+        assert_eq!("true", out.out);
+    });
+}
